f50491544dfcd2e6cb31aa5f9211e17f8b6612a5
Georgii Staroselskii <gosha371@gmail.com>
2017-11-25 22:49:52 +0200

    AirqualityDataFetcher: ErrorMsg parsed

--- src/AirqualityDataFetcher.hs
+++ src/AirqualityDataFetcher.hs
@@ -10,25 +10,26 @@ import Data.Monoid
 import Data.Aeson
 import GHC.Generics
 import Data.Text
+import qualified Data.ByteString.Lazy as B
 import Network.HTTP.Conduit
 
 type StationId = Int
 
 data MeasurementInfo =
     MeasurementInfo {
-            dta :: Float,
+--            data :: Float,
             time :: Int,
             en :: Text
     } deriving (Show, Generic)
 
 
 data NO2Data =
-     NO2Data { errorStatus :: Bool
+     NO2Data { error :: Bool
                ,latest :: MeasurementInfo
               } deriving (Show, Generic)
 
 data ErrorMsg =
-     ErrorMsg { errorStatus :: Bool
+     ErrorMsg { error :: Bool
                ,message :: !Text
               } deriving (Show, Generic)
 
@@ -43,17 +44,17 @@ requestBuilder :: StationId -> String -> String
 requestBuilder sid meas = apiUrl <> "?p=" <> meas <> "&ss=" <> (show sid) 
 
 
-getStationData :: StationId -> String -> IO (Maybe Value)
+getStationData :: StationId -> String -> IO B.ByteString
 getStationData sid meas = do
     rawJson <- simpleHttp $ requestBuilder sid meas
-    return (decode rawJson :: Maybe Value)
+    return rawJson 
 
 getStationNO2LevelJson :: Int -> IO String
 getStationNO2LevelJson stationId = do
-    response <- getStationData stationId "nitrogendioxide"
+    response <- (eitherDecode <$> getStationData stationId "nitrogendioxide") :: IO (Either String ErrorMsg)
     case response of
-                    (Just v) -> return (show $ v)
-                    Nothing -> return ""
+                    Right msg -> return (show $ msg)
+                    Left err -> return "err"
 
 parseNO2Level :: IO String -> IO String
 parseNO2Level json = do
