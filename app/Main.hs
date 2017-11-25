e50fbc00ee11de9f9c96c65adf308cb47b597036
soultoxik <maxim@prisma-ai.com>
2017-11-25 15:17:49 +0200

    renamed endpoints and specific station measurements 2

--- app/Main.hs
+++ app/Main.hs
@@ -16,10 +16,16 @@ import Types
 allStations :: Stations
 allStations = [S {stationId = 0, coord = XY 43.4 54.5, m = M [E "o3" 4.4] }]
 
+byId :: Int -> StationInfo -> Bool
+byId i (S {stationId = ci}) = (i == ci)
 
 main = do
   putStrLn "Starting Server..."
   let settings = setPort 3000 $ setHost "10.100.41.153" defaultSettings
   scottyOpts (Options {verbose = 1,  settings = settings}) $ do
-    get "/all" $ do
+    get "/stations" $ do
       json allStations
+
+    get "/station/:id" $ do
+      pid <- param "id"
+      json $ filter (byId pid) allStations
