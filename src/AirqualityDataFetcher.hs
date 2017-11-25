9299f16626a2f9da2a3fede521d71f188239147f
Georgii Staroselskii <gosha371@gmail.com>
2017-11-25 13:23:29 +0200

    AirqualityDataFetcher: add imports from an example from servant-client

---
+++ src/AirqualityDataFetcher.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module AirqualityDataFetcher
+    ( someFunc
+    ) where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Either
+import Data.Aeson
+import Data.Monoid
+import Data.Proxy
+import Data.Text (Text)
+import GHC.Generics
+import Servant.API
+import Servant.Client
+
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
